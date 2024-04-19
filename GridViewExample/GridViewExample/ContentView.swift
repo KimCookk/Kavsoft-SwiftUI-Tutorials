582a90f6a0f13fd23a55efe8b8b3a52bd6561848
Kim TaeSung <fopc4131@gmail.com>
2024-04-19 22:23:32 +0900

    new reposistory

---
+++ GridViewExample/GridViewExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GridViewExample
+//
+//  Created by 김태성 on 4/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GridViewExample/GridViewExample/GridViewExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridViewExampleApp.swift
+//  GridViewExample
+//
+//  Created by 김태성 on 4/19/24.
+//
+
+import SwiftUI
+
+@main
+struct GridViewExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
