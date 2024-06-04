655caa9423e505d492884e3c35d55ff7538741e6
Kim TaeSung <fopc4131@gmail.com>
2024-06-04 21:01:54 +0900

    Multiple Image Picker Example Update

--- MultipleImagePicker/MultipleImagePicker/ContentView.swift
+++ MultipleImagePicker/MultipleImagePicker/ContentView.swift
@@ -21,7 +21,6 @@ struct ContentView: View {
 struct HomeView: View {
     
     @State var selected: [UIImage] = []
-    @State var datas: [ImageData] = []
     @State var isShowImagePicker: Bool = false
     
     var body: some View {
@@ -29,6 +28,21 @@ struct HomeView: View {
             Color.black.opacity(0.07).edgesIgnoringSafeArea(.all)
             
             VStack {
+                
+                if !self.selected.isEmpty {
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(spacing: 20 ) {
+                            ForEach(self.selected, id: \.self) { i in
+                                Image(uiImage: i)
+                                    .resizable()
+                                    .frame(width: UIScreen.main.bounds.width - 40, height: 250)
+                                    .cornerRadius(15)
+                            }
+                        }
+                        .padding(.horizontal, 20)
+                    }
+                }
+                
                 Button(action: {
                     self.selected.removeAll()
                     isShowImagePicker = true
@@ -40,10 +54,11 @@ struct HomeView: View {
                 }
                 .background(.red)
                 .clipShape(Capsule())
+                .padding(.top, 25)
             }
             
             if isShowImagePicker {
-                ImagePickerView(selected: self.$selected, datas: self.$datas, isShow: self.$isShowImagePicker)
+                ImagePickerView(selected: self.$selected, isShow: self.$isShowImagePicker)
             }
            
         }
@@ -53,56 +68,99 @@ struct HomeView: View {
 struct ImagePickerView: View {
     
     @Binding var selected: [UIImage]
-    @Binding var datas: [ImageData]
     @Binding var isShow: Bool
     
+    @State var datas: [ImageData] = []
     @State var grid: [Int] = []
+    @State var disabled = false
     
     var body: some View {
-        
-        GeometryReader { geo in
-            VStack {
-                HStack {
-                    Text("Pick a Image")
-                        .fontWeight(.bold)
-                    
-                    Spacer()
-                }
-                .padding(.leading)
-                .padding(.top)
-                
-                if !self.datas.isEmpty {
-                    ScrollView(.vertical, showsIndicators: false) {
-                        VStack(spacing: 20) {
-                            ForEach(self.datas, id: \.self) { i in
-                                ForEach(i..<i+3, id:\.self) { j in
-                                    ImageCardView(data: self.datas[j])
-                                }
-                            }
+            GeometryReader { geo in
+                VStack {
+                    if !self.grid.isEmpty {
+                        headerView()
+                        scrollViewContent()
+                        
+                        Button(action: {
+                            self.isShow.toggle()
+                        }) {
+                            Text("Select")
+                                .foregroundColor(.white)
+                                .padding(.vertical, 10)
+                                .frame(width: UIScreen.main.bounds.width / 2)
+                        }
+                        .background(Color.red.opacity(self.selected.count != 0 ? 1 : 0.5))
+                        .clipShape(Capsule())
+                        .padding(.bottom, 25)
+                        .disabled(self.selected.count != 0 ? false : true)
+                    } else {
+                        if self.disabled {
+                            Text("Enabled Storage Access In Settings !!!!")
+                        } else {
+                            IndicatorView()
                         }
                     }
                 }
+                .frame(width: UIScreen.main.bounds.width - 80, height: UIScreen.main.bounds.height / 1.5)
+                .background(Color.white)
+                .cornerRadius(12)
+                .position(x: geo.size.width / 2, y: geo.size.height / 2) // Center the VStack
+
             }
-            .frame(width: UIScreen.main.bounds.width - 80, height: UIScreen.main.bounds.height / 1.5)
-            .background(.white)
-            .cornerRadius(12)
-        }
-        .background(Color.black.opacity(0.1).edgesIgnoringSafeArea(.all))
-        .onTapGesture {
-            self.isShow.toggle()
-        }
-        .onAppear {
-            PHPhotoLibrary.requestAuthorization { (status) in
-                if status == .authorized {
-                    
-                    self.getAllImages()
-                } else {
-                    
-                    print("not authorized")
+            .background(Color.black.opacity(0.1).edgesIgnoringSafeArea(.all))
+            .onTapGesture {
+                
+                self.isShow.toggle()
+            }
+            .onAppear {
+                PHPhotoLibrary.requestAuthorization { (status) in
+                    if status == .authorized {
+                        self.getAllImages()
+                        self.disabled = false
+                    } else {
+                        print("not authorized")
+                        self.disabled = true
+                    }
                 }
             }
         }
-    }
+        
+        @ViewBuilder
+        private func headerView() -> some View {
+            HStack {
+                Text("Pick a Image")
+                    .fontWeight(.bold)
+                Spacer()
+            }
+            .padding(.leading)
+            .padding(.top)
+        }
+        
+        @ViewBuilder
+        private func scrollViewContent() -> some View {
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack(spacing: 20) {
+                    ForEach(0..<self.grid.count, id: \.self) { i in
+                        rowContent(i: i)
+                    }
+                }
+            }
+        }
+        
+        @ViewBuilder
+        private func rowContent(i: Int) -> some View {
+            HStack(spacing: 8) {
+                ForEach(i..<i+3, id: \.self) { j in
+                    if j < self.datas.count {
+                        ImageCardView(data: self.datas[j], selected: self.$selected)
+                    }
+                }
+                if self.datas.count % 3 != 0 && i == self.grid.last! {
+                    Spacer()
+                }
+            }
+            .padding(.leading, (self.datas.count % 3 != 0 && i == self.grid.last!) ? 15 : 0)
+        }
     
     func getAllImages() {
         let req = PHAsset.fetchAssets(with: .image, options: .none)
@@ -143,12 +201,60 @@ struct ImageData: Hashable {
 
 struct ImageCardView: View {
     
-    var data: ImageData
+    @State var data: ImageData
+    @Binding var selected: [UIImage]
     
     var body: some View {
         
-        Image(uiImage: self.data.image)
-            .resizable()
-            .frame(width: (UIScreen.main.bounds.width - 80) / 3, height: 90)
+        ZStack {
+            Image(uiImage: self.data.image)
+                .resizable()
+            
+            if self.data.selected {
+                ZStack {
+                    Color.black.opacity(0.5)
+                    
+                    Image(systemName: "checkmark")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .foregroundColor(.white)
+                }
+            }
+        }
+        .frame(width: (UIScreen.main.bounds.width - 80) / 3, height: 90)
+        .onTapGesture {
+            
+            if !self.data.selected {
+                
+                self.data.selected = true
+                
+                self.selected.append(self.data.image)
+                
+            } else {
+                
+                for i in 0..<self.selected.count {
+                    
+                    self.selected.remove(at: i)
+                    self.data.selected = false
+                    
+                    return
+                }
+            }
+        }
+    }
+}
+
+struct IndicatorView: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> UIActivityIndicatorView {
+        
+        let view = UIActivityIndicatorView(style: .large)
+        view.startAnimating()
+        
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIActivityIndicatorView, context: Context) {
+        
     }
 }
